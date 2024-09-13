b19c390b49b29583f7d70728b88acc69fa0817f5
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-09-13 16:37:03 +0700

    update function

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function createElement(element, attribute, inner) {
+  if (typeof element === "undefined") {
+    return false;
+  }
+  if (typeof inner === "undefined") {
+    inner = "";
+  }
+  var el = document.createElement(element);
+  if (typeof attribute === "object") {
+    for (var key in attribute) {
+      el.setAttribute(key, attribute[key]);
+    }
+  }
+  if (!Array.isArray(inner)) {
+    inner = [inner];
+  }
+  for (var k = 0; k < inner.length; k++) {
+    if (inner[k].tagName) {
+      el.appendChild(inner[k]);
+    } else {
+      el.appendChild(document.createTextNode(inner[k]));
+    }
+  }
+  return el;
+}
+
+$(document).ready(function () {
+  mainContainer = document.getElementById("main-container");
+});
