10d9d17cccc8c08660526b889edc3f5b801c279f
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-09-16 15:34:57 +0700

    Seperate main code to main.js

--- script.js
+++ script.js
@@ -101,34 +101,3 @@ const StudentTable = function (tableName) {
   Table.call(this, tableName);
 };
 StudentTable.prototype = Object.create(Table.prototype);
-
-document.addEventListener("DOMContentLoaded", function () {
-  const mainContainer = document.getElementById("main-container");
-
-  const section = new Section("student", "Student Name Table");
-  const studentTable = new StudentTable("student-table");
-  section.appendChild(studentTable);
-  studentTable.createHeaderRow([
-    "No.",
-    "Student Name",
-    "Student ID",
-    "Nick Name",
-    "Comments",
-  ]);
-
-  fetch("students.json")
-    .then((res) => res.json())
-    .then(function (data) {
-      for (let i = 0; i < data.length; i++) {
-        studentTable.createDataRow([
-          i + 1,
-          data[i].name,
-          data[i].studentId,
-          data[i].nickname,
-          data[i].comment,
-        ]);
-      }
-    });
-
-  section.addSelfToParent(mainContainer);
-});
