e926982214985d562a662e07ae5c2b04bd102b22
Mr.Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-09-16 15:30:11 +0700

    add JavaScript and JSON data file

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+////////////////////////////////////////
+// Helper Functions
+////////////////////////////////////////
 function createElement(element, attribute, inner) {
   if (typeof element === "undefined") {
     return false;
@@ -26,7 +29,106 @@ function createElement(element, attribute, inner) {
   return el;
 }
 
+////////////////////////////////////////
+// Section Div
+////////////////////////////////////////
+const CustomElement = function (elementName, elementId) {
+  this.elementId = elementId;
+  this.innerElement = createElement(elementName, {
+    id: elementId,
+  });
+};
+
+CustomElement.prototype.getElement = function () {
+  return this.innerElement;
+};
+
+CustomElement.prototype.appendChild = function (element) {
+  if (element instanceof CustomElement) {
+    this.innerElement.appendChild(element.getElement());
+  } else {
+    this.innerElement.appendChild(element);
+  }
+};
+
+CustomElement.prototype.addSelfToParent = function (parentElement) {
+  if (parentElement instanceof HTMLElement) {
+    parentElement.appendChild(this.getElement());
+  } else {
+    throw new Error("Invalid parent element type");
+  }
+};
+
+const Section = function (name, title) {
+  CustomElement.call(this, "section", `section-${name}`);
+  this.createHeader(title);
+};
+Section.prototype = Object.create(CustomElement.prototype);
+
+Section.prototype.createHeader = function (title) {
+  const headerContainer = createElement("div", {
+    id: `${this.elementId}-header`,
+  });
+  headerContainer.appendChild(createElement("h1", {}, title));
+  this.appendChild(headerContainer);
+};
+
+////////////////////////////////////////
+// Section Div
+////////////////////////////////////////
+const Table = function (tableName) {
+  CustomElement.call(this, "table", `table-${tableName}`);
+};
+Table.prototype = Object.create(CustomElement.prototype);
+
+Table.prototype.createHeaderRow = function (headers) {
+  const headerRow = createElement("tr");
+  for (let i = 0; i < headers.length; i++) {
+    headerRow.appendChild(createElement("th", {}, headers[i]));
+  }
+  this.appendChild(headerRow);
+};
+
+Table.prototype.createDataRow = function (dataArray) {
+  const dataRow = createElement("tr");
+  for (let i = 0; i < dataArray.length; i++) {
+    dataRow.appendChild(createElement("td", {}, dataArray[i]));
+  }
+  this.appendChild(dataRow);
+};
+
+const StudentTable = function (tableName) {
+  Table.call(this, tableName);
+};
+StudentTable.prototype = Object.create(Table.prototype);
+
 document.addEventListener("DOMContentLoaded", function () {
   const mainContainer = document.getElementById("main-container");
-  mainContainer.appendChild(createElement("h1", {}, "Student Name Table"));
+
+  const section = new Section("student", "Student Name Table");
+  const studentTable = new StudentTable("student-table");
+  section.appendChild(studentTable);
+  studentTable.createHeaderRow([
+    "No.",
+    "Student Name",
+    "Student ID",
+    "Nick Name",
+    "Comments",
+  ]);
+
+  fetch("students.json")
+    .then((res) => res.json())
+    .then(function (data) {
+      for (let i = 0; i < data.length; i++) {
+        studentTable.createDataRow([
+          i + 1,
+          data[i].name,
+          data[i].studentId,
+          data[i].nickname,
+          data[i].comment,
+        ]);
+      }
+    });
+
+  section.addSelfToParent(mainContainer);
 });
